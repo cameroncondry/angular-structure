6910d3de5dd43e7b073a3f37da62d7d0d0d3ea1e
Alain Cartaya Delgado <alcartdelg@gmail.com>
2017-08-23 12:53:53 -0400

    added zooming functionality

--- src/app/components/campaign/campaign.component.ts
+++ src/app/components/campaign/campaign.component.ts
@@ -1,11 +1,7 @@
-import {Component,Output,EventEmitter,Renderer2,ViewChild, OnInit} from "@angular/core";
+import {Component} from "@angular/core";
 import {ZoomDirective} from "../../directives/zoom.directive"
 
 @Component({
     templateUrl: './campaign.component.html',
 })
-export class CampaignComponent{
-  constructor( private renderer:Renderer2 ) {};
-
-
-}
+export class CampaignComponent { }
